17dd6d2c097ff379c8012091725e7d8283ba7e9b
Rgarg6272 <73298337+Rgarg6272@users.noreply.github.com>
2023-08-10 15:02:17 +0530

    Update materialtable.js

--- materialtable.js
+++ materialtable.js
@@ -1,122 +1,76 @@
-import React, { useState } from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import Modal from "@material-ui/core/Modal";
-import ExitToAppIcon from "@material-ui/icons/ExitToApp";
-import MaterialTable from "material-table";
-import Button from "@material-ui/core/Button";
-import CloseIcon from "@material-ui/icons/Close";
-
-const useStyles = makeStyles((theme) => ({
-  modal: {
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  paper: {
-    position: "relative",
-    width: "90%",
-    maxWidth: 400,
-    backgroundColor: theme.palette.background.paper,
-    boxShadow: theme.shadows[5],
-    padding: theme.spacing(2, 4, 3),
-    display: "flex",
-    flexDirection: "column",
-  },
-
-  closeButton: {
-    position: "absolute",
-    cursor: "pointer",
-    top: theme.spacing(1),
-    right: theme.spacing(1),
-  },
-}));
-
-export default function SimpleModal() {
-  const data = [
-    {
-      name: "John Doe",
-      age: 30,
-      gender: "Male",
-      delegated: "View details",
-    },
-  ];
-  const columns = [
-    { title: "Name", field: "name" },
-    { title: "Age", field: "age", type: "numeric" },
-    { title: "Gender", field: "gender" },
-    {
-      title: "Bio Data",
-      field: "delegated",
-      render: (rowData) => (
-        <div
-          style={{
-            display: "flex",
-            alignItems: "center",
-          }}
-        >
-          <a style={{ cursor: "pointer" }} onClick={() => handleOpen(rowData)}>
-            <span
-              style={{
-                marginRight: "5px",
-                position: "relative",
-                bottom: "6px",
-                textDecoration: "underline",
-              }}
-            >
-              View Details
-            </span>
-            <ExitToAppIcon
-              style={{ cursor: "pointer" }}
-              onClick={() => handleOpen(rowData)}
-            />
-          </a>
-        </div>
-      ),
-    },
-  ];
-
-  const classes = useStyles();
-  const [open, setOpen] = useState(false);
-
-  const handleOpen = () => {
-    setOpen(true);
-  };
-
-  const handleClose = () => {
-    setOpen(false);
-  };
-
-  const body = (
-    <div className={classes.paper}>
-      <CloseIcon className={classes.closeButton} onClick={handleClose} />
-      <h2 id="simple-modal-title">Text in a modal</h2>
-      <p id="simple-modal-description">
-        Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-      </p>
-      <Button variant="contained" color="primary" onClick={handleClose}>
-        Close
-      </Button>
-    </div>
-  );
-
-  return (
-    <div>
-      <MaterialTable
-        title="User Data"
-        columns={columns}
-        data={data}
-        options={{ search: false, paging: false, sorting: false }}
-      />
-
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="simple-modal-title"
-        aria-describedby="simple-modal-description"
-        className={classes.modal}
-      >
-        {body}
-      </Modal>
-    </div>
-  );
-}
+ {
+                                title: "Delegated Unreachable",
+                                field: "Dele_Unreachable",
+                                cellStyle: {
+                                    color: "#861426",
+                                    fontSize: commonFontSizes.bodyTwo + "rem",
+                                    textDecoration: "underline",
+                                    fontWeight: 600,
+                                    // minWidth: 110,
+                                    // maxWidth: 110,
+                                    whiteSpace: "nowrap",
+                                },
+                                render: (rowData) => (
+                                    <div style={{ display: "flex", flexDirection: "row", justifyContent: "left", alignItems: "left" }} onClick={handleDelegatedPopup}>
+                                        {rowData.Dele_Unreachable}
+                                        <ExitToAppIcon style={{ width: "1.2rem", height: "1.2rem", paddingLeft: "0.5rem", color: "#861426" }} />
+                                    </div>
+                                ),
+                            },
+                            {
+                                title: "Delegated Care Coordination Refuced",
+                                field: "Dele_Refuced",
+                                cellStyle: {
+                                    color: "#861426",
+                                    fontSize: commonFontSizes.bodyTwo + "rem",
+                                    textDecoration: "underline",
+                                    fontWeight: 600,
+                                    // minWidth: 110,
+                                    // maxWidth: 110,
+                                    whiteSpace: "nowrap",
+                                },
+                                render: (rowData) => (
+                                    <div style={{ display: "flex", flexDirection: "row", justifyContent: "left", alignItems: "left" }} >
+                                        {rowData.Dele_Refuced}
+                                        <ExitToAppIcon style={{ width: "1.2rem", height: "1.2rem", paddingLeft: "0.5rem", color: "#861426" }} />
+                                    </div>
+                                ),
+                            },
+                            {
+                                title: "Delegated CNA Refuced",
+                                field: "Dele_Cna_Refuced",
+                                cellStyle: {
+                                    color: "#861426",
+                                    fontSize: commonFontSizes.bodyTwo + "rem",
+                                    textDecoration: "underline",
+                                    fontWeight: 600,
+                                    // minWidth: 110,
+                                    // maxWidth: 110,
+                                    whiteSpace: "nowrap",
+                                },
+                                render: (rowData) => (
+                                    <div style={{ display: "flex", flexDirection: "row", justifyContent: "left", alignItems: "left" }}>
+                                        {rowData.Dele_Cna_Refuced}
+                                        <ExitToAppIcon style={{ width: "1.2rem", height: "1.2rem", paddingLeft: "0.5rem", color: "#861426" }} />
+                                    </div>
+                                ),
+                            },
+                            {
+                                title: "Delegated Touchpoint",
+                                field: "Dele_Touchpoint",
+                                cellStyle: {
+                                    color: "#861426",
+                                    fontSize: commonFontSizes.bodyTwo + "rem",
+                                    textDecoration: "underline",
+                                    fontWeight: 600,
+                                    // minWidth: 110,
+                                    // maxWidth: 110,
+                                    whiteSpace: "nowrap",
+                                },
+                                render: (rowData) => (
+                                    <div style={{ display: "flex", flexDirection: "row", justifyContent: "left", alignItems: "left" }}>
+                                        {rowData.Dele_Touchpoint}
+                                        <ExitToAppIcon style={{ width: "1.2rem", height: "1.2rem", paddingLeft: "0.5rem", color: "#861426" }} />
+                                    </div>
+                                ),
+                            },
