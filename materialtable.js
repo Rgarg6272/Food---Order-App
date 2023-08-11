3c09c158be36978ab66f07862f719fd93d34c5ba
Rgarg6272 <73298337+Rgarg6272@users.noreply.github.com>
2023-08-11 11:33:16 +0530

    Update materialtable.js

--- materialtable.js
+++ materialtable.js
@@ -1,76 +1,217 @@
- {
-                                title: "Delegated Unreachable",
-                                field: "Dele_Unreachable",
-                                cellStyle: {
-                                    color: "#861426",
-                                    fontSize: commonFontSizes.bodyTwo + "rem",
-                                    textDecoration: "underline",
-                                    fontWeight: 600,
-                                    // minWidth: 110,
-                                    // maxWidth: 110,
-                                    whiteSpace: "nowrap",
-                                },
-                                render: (rowData) => (
-                                    <div style={{ display: "flex", flexDirection: "row", justifyContent: "left", alignItems: "left" }} onClick={handleDelegatedPopup}>
-                                        {rowData.Dele_Unreachable}
-                                        <ExitToAppIcon style={{ width: "1.2rem", height: "1.2rem", paddingLeft: "0.5rem", color: "#861426" }} />
-                                    </div>
-                                ),
-                            },
-                            {
-                                title: "Delegated Care Coordination Refuced",
-                                field: "Dele_Refuced",
-                                cellStyle: {
-                                    color: "#861426",
-                                    fontSize: commonFontSizes.bodyTwo + "rem",
-                                    textDecoration: "underline",
-                                    fontWeight: 600,
-                                    // minWidth: 110,
-                                    // maxWidth: 110,
-                                    whiteSpace: "nowrap",
-                                },
-                                render: (rowData) => (
-                                    <div style={{ display: "flex", flexDirection: "row", justifyContent: "left", alignItems: "left" }} >
-                                        {rowData.Dele_Refuced}
-                                        <ExitToAppIcon style={{ width: "1.2rem", height: "1.2rem", paddingLeft: "0.5rem", color: "#861426" }} />
-                                    </div>
-                                ),
-                            },
-                            {
-                                title: "Delegated CNA Refuced",
-                                field: "Dele_Cna_Refuced",
-                                cellStyle: {
-                                    color: "#861426",
-                                    fontSize: commonFontSizes.bodyTwo + "rem",
-                                    textDecoration: "underline",
-                                    fontWeight: 600,
-                                    // minWidth: 110,
-                                    // maxWidth: 110,
-                                    whiteSpace: "nowrap",
-                                },
-                                render: (rowData) => (
-                                    <div style={{ display: "flex", flexDirection: "row", justifyContent: "left", alignItems: "left" }}>
-                                        {rowData.Dele_Cna_Refuced}
-                                        <ExitToAppIcon style={{ width: "1.2rem", height: "1.2rem", paddingLeft: "0.5rem", color: "#861426" }} />
-                                    </div>
-                                ),
-                            },
-                            {
-                                title: "Delegated Touchpoint",
-                                field: "Dele_Touchpoint",
-                                cellStyle: {
-                                    color: "#861426",
-                                    fontSize: commonFontSizes.bodyTwo + "rem",
-                                    textDecoration: "underline",
-                                    fontWeight: 600,
-                                    // minWidth: 110,
-                                    // maxWidth: 110,
-                                    whiteSpace: "nowrap",
-                                },
-                                render: (rowData) => (
-                                    <div style={{ display: "flex", flexDirection: "row", justifyContent: "left", alignItems: "left" }}>
-                                        {rowData.Dele_Touchpoint}
-                                        <ExitToAppIcon style={{ width: "1.2rem", height: "1.2rem", paddingLeft: "0.5rem", color: "#861426" }} />
-                                    </div>
-                                ),
-                            },
+import React, { useState } from "react";
+import "./App.css";
+import { makeStyles } from "@material-ui/core/styles";
+import MaterialTable from "material-table";
+import SearchIcon from "@material-ui/icons/Search";
+import ExitToAppIcon from "@material-ui/icons/ExitToApp";
+import {
+  useMediaQuery,
+  createMuiTheme,
+  MuiThemeProvider,
+} from "@material-ui/core";
+import DelegatedDialog from "./component/DelegatedDialog";
+
+const useStyles = makeStyles((theme) => ({
+  modal: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  paper: {
+    position: "absolute",
+    width: "90%",
+    maxWidth: 400,
+    backgroundColor: theme.palette.background.paper,
+    boxShadow: theme.shadows[5],
+    padding: "0px 30px 30px",
+    display: "flex",
+    flexDirection: "column",
+  },
+
+  closeButton: {
+    position: "absolute",
+    cursor: "pointer",
+    color: "#A71930",
+  },
+
+  btn: {
+    "&:hover": {
+      background: "#14837B",
+    },
+    position: "relative",
+    left: "82%",
+    top: "15px",
+    backgroundColor: "#14837B",
+    textTransform: "capitalize",
+    color: "white",
+    borderRadius: 0,
+    minWidth: "80px",
+  },
+}));
+
+const App = () => {
+  const [tableData, setTableData] = useState([
+    {
+      city: "Chennai",
+      birthday: "20-04-2001",
+      Fee: 22000,
+      bio: "Bio-data",
+    },
+  ]);
+
+  const columns = [
+    {
+      title: "City",
+      field: "city",
+      align: "center",
+      filtering: false,
+      render: (rowData) => (
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+          }}
+        >
+          <a style={{ cursor: "pointer" }} onClick={handleDialog}>
+            <span
+              style={{
+                marginRight: "5px",
+                position: "relative",
+                bottom: "6px",
+                textDecoration: "underline",
+              }}
+            >
+              View Details
+            </span>
+            <ExitToAppIcon style={{ cursor: "pointer" }} />
+          </a>
+        </div>
+      ),
+    },
+
+    {
+      title: "School Fees",
+      field: "Fee",
+      type: "currency",
+      currencySetting: { currencyCode: "INR", minimumFractionDigits: 0 },
+      align: "center",
+      filtering: false,
+      render: (rowData) => (
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+          }}
+        >
+          <a style={{ cursor: "pointer" }} onClick={handleDialog}>
+            <span
+              style={{
+                marginRight: "5px",
+                position: "relative",
+                bottom: "6px",
+                textDecoration: "underline",
+              }}
+            >
+              View Details
+            </span>
+            <ExitToAppIcon style={{ cursor: "pointer" }} />
+          </a>
+        </div>
+      ),
+    },
+    {
+      title: "Bio-Data",
+      field: "bio",
+      align: "center",
+      filtering: false,
+      render: (rowData) => (
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+          }}
+        >
+          <a style={{ cursor: "pointer" }} onClick={handleDialog}>
+            <span
+              style={{
+                marginRight: "5px",
+                position: "relative",
+                bottom: "6px",
+                textDecoration: "underline",
+              }}
+            >
+              View Details
+            </span>
+            <ExitToAppIcon style={{ cursor: "pointer" }} />
+          </a>
+        </div>
+      ),
+    },
+  ];
+
+  const theme = createMuiTheme();
+  const isDesktop = useMediaQuery(theme.breakpoints.up("sm"));
+  const [dialogOpen, setDialogOpen] = useState(false);
+
+  const handleDialog = () => {
+    setDialogOpen(true);
+  };
+
+  const handleCloseDialog = () => {
+    setDialogOpen(false);
+  };
+
+  return (
+    <>
+      <MuiThemeProvider theme={theme}>
+        <div className="App">
+          <h1 align="center">Material Table</h1>
+          <MaterialTable
+            columns={columns}
+            data={tableData}
+            title="Student Information"
+            editable={{
+              onRowAdd: (newRow) =>
+                new Promise((resolve, reject) => {
+                  setTableData([...tableData, newRow]);
+                  resolve();
+                }),
+            }}
+            icons={{
+              Filter: () => (
+                <SearchIcon
+                  style={{
+                    position: "absolute",
+                    right: 8,
+                    top: 8,
+                  }}
+                />
+              ),
+            }}
+            options={{
+              sorting: true,
+              searchAutoFocus: true,
+              searchFieldVariant: "outlined",
+              rowStyle: (data, index) =>
+                index % 2 === 0
+                  ? { backgroundColor: "lightgreen" }
+                  : { backgroundColor: "#fff" },
+              cellStyle: { border: "1px solid lightgrey" },
+              headerStyle: { border: "1px solid lightgrey" },
+              tableLayout: isDesktop ? "auto" : "fixed",
+              addRowPosition: "first",
+              actionsColumnIndex: -1,
+              filtering: true,
+              padding: "dense",
+            }}
+          />
+          {dialogOpen && (
+            <DelegatedDialog handleCloseDialog={handleCloseDialog} />
+          )}
+        </div>
+      </MuiThemeProvider>
+    </>
+  );
+};
+
+export default App;
