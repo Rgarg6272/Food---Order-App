044a021f00226f9e5584440e0652085f7eb85fa7
Rgarg6272 <73298337+Rgarg6272@users.noreply.github.com>
2023-08-18 13:05:29 +0530

    Update dashboarddata.js

--- dashboarddata.js
+++ dashboarddata.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { Doughnut } from "react-chartjs-2";
 import {
   Grid,
   Container,
@@ -7,214 +7,133 @@ import {
   TextField,
   Typography,
 } from "@material-ui/core";
-import { inquiriesStyles } from "../../../css/InquiriesCss";
-import { commonFontSizes } from "../../../css/FontSizes";
-import {
-  donutChartData,
-  InquiriesData,
-  InquiriesProcessorLegend,
-} from "../../../../constants/inquiriesData";
-import { DoughnutChart } from "../../../common/charts/DoghnutChart";
-import { useEffect } from "react";
-
-import StopIcon from "@material-ui/icons/Stop";
-const useStyles = makeStyles((theme) => inquiriesStyles(theme));
-const useStyles2 = makeStyles((theme) => ({
-  DoughnutChartHeight: {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    height: "300px",
-
+// import { inquiriesStyles } from "../../css/InquiriesCss";
+const useStyles = makeStyles((theme) => ({
+  DoughnutChartWidth: {
+    width: "35%",
     [theme.breakpoints.down(2100)]: {
-      height: "300px", //67%
+      width: "35%", //67%
     },
 
     [theme.breakpoints.down("lg")]: {
-      height: "300px", //75%
+      width: "50%", //75%
     },
 
     [theme.breakpoints.down(1750)]: {
-      height: "300px",//80%
+      width: "50%", //80%
     },
 
     [theme.breakpoints.down(1550)]: {
-      height: "300px", //90%
+      width: "50%", //90%
     },
 
     [theme.breakpoints.down(1400)]: {
-      height: "300px",
+      width: "50%",
     },
-    [theme.breakpoints.down('md')]: {
-
-      height: "400px",
-
-    }
-
+    [theme.breakpoints.down("md")]: {
+      width: "60%",
+    },
+    [theme.breakpoints.down("sm")]: {
+      width: "50%",
+    },
   },
 }));
-export const InquiryDashboardData = (props) => {
-  const { inquriesData } = props;
-  const [newCount, setNew] = useState(
-    inquriesData ? inquriesData.New_Inquiries_count : ""
-  );
-  const [resolved, setResolved] = useState(
-    inquriesData ? inquriesData.Resloved_Inquiries_count : ""
-  );
-  const [inprogress, setInprogress] = useState(
-    inquriesData ? inquriesData.Inprogress_Inquiries_count : ""
-  );
-  const [all, setAll] = useState(
-    inquriesData ? inquriesData.All_Inquiries_count : ""
-  );
-  const [date, setDate] = useState(
-    inquriesData ? inquriesData.User_Login_Date : ""
-  );
+export const DoughnutChart = ({
+  title,
+  legendPos,
+  legendDis,
+  cutout,
+  width,
+  height,
+  count,
+  countPosY,
+  totalPosY,
+  chartConfig,
+}) => {
   const classes = useStyles();
-  const classes2 = useStyles2();
-  const [donutChartData, setDonutChartData] = useState({
-    labels: [],
-    datasets: [
-      {
-        backgroundColor: [
-          "rgba(255, 99, 132, 0.2)",
-          "rgba(54, 162, 235, 0.2)",
-          "rgba(255, 206, 86, 0.2)",
-          "rgba(75, 192, 192, 0.2)",
-          "rgba(153, 102, 255, 0.2)",
-          "rgba(255, 159, 64, 0.2)",
-        ],
-        borderColor: [
-          "rgba(255, 99, 132, 1)",
-          "rgba(54, 162, 235, 1)",
-          "rgba(255, 206, 86, 1)",
-          "rgba(75, 192, 192, 1)",
-          "rgba(153, 102, 255, 1)",
-          "rgba(255, 159, 64, 1)",
-        ],
-        borderWidth: 1,
-      },
-    ],
-  });
 
-  useEffect(() => {
-    setNew(inquriesData ? inquriesData.New_Inquiries_count : "");
-    setResolved(inquriesData ? inquriesData.Resloved_Inquiries_count : "");
-    setInprogress(inquriesData ? inquriesData.Inprogress_Inquiries_count : "");
-    setAll(inquriesData ? inquriesData.All_Inquiries_count : "");
-    setDate(inquriesData ? inquriesData.User_Login_Date : "");
-  }, [inquriesData]);
+  let fontSize = 1;
+  let donutX;
+  let donutY = 0;
+  let cFontWeight = 500;
 
-  const data = {
-    labels: ["New", "Resolved", "Inprogress"],
-    datasets: [
-      {
-        label: "# of Votes",
-        data: [newCount, resolved, inprogress],
-        backgroundColor: ["#E18210", "#29A1C6", "#A71930"],
-        // cutout:'90%'
-      },
-    ],
-  };
+  if (title && title.includes("DAILY")) {
+    fontSize = (height / 200).toFixed(2);
+    donutX = "DAILY";
+    donutY = 0.5 * height;
+    cFontWeight = 500;
+  } else if (title && title.includes("STAGE")) {
+    fontSize = (height / 300).toFixed(2);
+    donutX = "STAGING";
+    donutY = 0.59 * height;
+    cFontWeight = 400;
+  } else if (title && title.includes("RESPONSIBILITY")) {
+    fontSize = (height / 250).toFixed(2);
+    donutX = "RESPONSIBILITY";
+    donutY = 0.57 * height;
+    cFontWeight = 400;
+  } else {
+    fontSize = (height / 170).toFixed(2);
+    donutX = "DEFAULT";
+    donutY = 0.5 * height;
+    cFontWeight = 500;
+  }
 
-  const config = {
-    type: "doughnut",
-    data,
-
-    // plugins : [doughnutLabelsLine]
-  };
-  const options = {
-    type: "outlabeledDoughnut",
-    responsive: true,
-    maintainAspectRatio: true,
-    // elements: {
-    //   center: {
-    //     text: "平均年齢",
-    //     color: "#FF6384",
-    //     fontStyle: "Arial",
-    //     sidePadding: 0,
-    //     minFontSize: 15,
-    //     lineHeight: 15
-    //   }
-    // },
-    // plugins: {
-    //   legend: true,
-    //   outlabels: {
-    //     text: "%p\n%v万人",
-    //     color: "white",
-    //     stretch: 40,
-    //     font: {
-    //       resizable: true,
-    //       minSize: 10,
-    //       maxSize: 12
-    //     }
-    //   }
-    // },
-    tooltips: {
-      enabled: true,
-    },
-  };
-
-  const handleDate = () => {
-    const today = new Date();
-    return today;
-  };
   return (
-    <Container className={classes.inquiryDataDashboard}>
-      <Grid container>
-        <Grid
-          item
-          xs={12}
-          style={{
-            display: "flex",
-            justifyContent: "right",
-            paddingRight: "20px",
-          }}
-        >
-          <Typography style={{ fontSize: "0.875rem", color: "#000000", paddingTop: '1%' }}>
-            Status shown as on today
-          </Typography>
-        </Grid>
-        {/* <span style={{ fontSize: "0.875rem", color: "#000000", display: 'flex', justifyContent: "right", width: '100%' }}>Status shown as on today</span> */}
-        {all && (
-          <Grid
-            item
-            xs={12}
-            sm={12}
-            md={12}
-            lg={12}
-            xl={12}
-            style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}
-          >
-            <div style={{ width: '20%', marginLeft: '20px' }}>
-              {InquiriesProcessorLegend.map((item) => (
-                <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'flex-start', marginRight: '1rem' }}>
-                  <p>{item.label}</p>
-                  <StopIcon
-                    style={{ color: item.color, verticalAlign: "middle" }}
-                  />
-                  <p>{item.id === "New" ? newCount : item.id === "Inprogress" ? inprogress : resolved}</p>
-                </div>
-              ))}
-            </div>
-            <div style={{ width: '80%' }} className={classes2.DoughnutChartHeight}>
-              <DoughnutChart
-                count={all}
-                legendDis={false}
-                cutout={6}
-                width={400}
-                height={250}
-                countPosY={-70}
-                totalPosY={0}
-                countPosX={0}
-                totalPosX={40}
-                chartConfig={data}
-              />
-            </div>
-          </Grid>
-        )}
-      </Grid>
-    </Container>
+    <div
+      className={classes.DoughnutChartWidth}
+      //   style={{ width: "35%" }}
+    >
+      <Doughnut
+        data={chartConfig}
+        options={{
+          cutout: "60%",
+          radius: "100%",
+          responsive: true,
+          maintainAspectRatio: true,
+          plugins: {
+            title: {
+              display: title ? true : false,
+              text: title,
+              textAlign: "left",
+            },
+            legend: {
+              labels: {
+                usePointStyle: true,
+                boxWidth: 9,
+              },
+              display: false,
+              // position: legendPos
+            },
+          },
+        }}
+        plugins={[
+          {
+            beforeDraw: function (chart) {
+              const width = chart.width,
+                //height = chart.height
+                ctx = chart.ctx;
+              ctx.restore();
+              ctx.font = 1 + "em sans-serif";
+              ctx.textAlign = "center";
+              ctx.textBaseLine = "start";
+
+              // Calculate text position
+
+              const textX = Math.round(width / 2);
+              const textY = donutY - totalPosY;
+              const countTextX = count ? textY + 80 + countPosY : textY;
+              const countTextY = count ? textY + 110 + countPosY : textY;
+              // Draw the text
+
+              ctx.fillText("All Inquiries", textX, textY);
+              ctx.fontWeight = cFontWeight;
+              ctx.fillText(count, textX, countTextY, countTextX);
+              ctx.save();
+            },
+          },
+        ]}
+      />
+    </div>
   );
 };
-export default InquiryDashboardData;
