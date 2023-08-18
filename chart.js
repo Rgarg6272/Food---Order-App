33bf89c0f5e2fd6cbdb112538b8af6d5b377b3f9
Rgarg6272 <73298337+Rgarg6272@users.noreply.github.com>
2023-08-18 17:31:27 +0530

    Update chart.js

--- chart.js
+++ chart.js
@@ -1,86 +1,144 @@
-import { Doughnut } from 'react-chartjs-2';
+import { Doughnut } from "react-chartjs-2";
+import {
+  Grid,
+  Container,
+  makeStyles,
+  Card,
+  TextField,
+  Typography,
+} from "@material-ui/core";
+// import { inquiriesStyles } from "../../css/InquiriesCss";
+const useStyles = makeStyles((theme) => ({
+  DoughnutChartWidth: {
+    width: "35%",
+    [theme.breakpoints.down(2100)]: {
+      width: "35%", //67%
+    },
 
-export const DoughnutChart = ({ title, legendPos, legendDis, cutout, width, height, count, countPosY, totalPosY, chartConfig }) => {
-    let fontSize = 1;
-    let donutX;
-    let donutY = 0;
-    let cFontWeight = 500;
+    [theme.breakpoints.down("lg")]: {
+      width: "60%", //75%
+    },
 
-    if (title && title.includes("DAILY")) {
-        fontSize = (height / 200).toFixed(2);
-        donutX = "DAILY";
-        donutY = 0.5 * height;;
-        cFontWeight = 500;
+    [theme.breakpoints.down(1750)]: {
+      width: "60%", //80%
+    },
 
-    }
-    else if (title && title.includes("STAGE")) {
-        fontSize = (height / 300).toFixed(2);
-        donutX = "STAGING"
-        donutY = 0.59 * height;
-        cFontWeight = 400;
-    }
-    else if (title && title.includes("RESPONSIBILITY")) {
-        fontSize = (height / 250).toFixed(2);
-        donutX = "RESPONSIBILITY";
-        donutY = 0.57 * height;
-        cFontWeight = 400;;
-    }
-    else {
-        fontSize = (height / 170).toFixed(2);
-        donutX = "DEFAULT";
-        donutY = 0.5 * height;
-        cFontWeight = 500;;
-    }
+    [theme.breakpoints.down(1550)]: {
+      width: "60%", //90%
+    },
+
+    [theme.breakpoints.down(1400)]: {
+      width: "60%",
+    },
+    [theme.breakpoints.down("md")]: {
+      width: "60%",
+    },
+    [theme.breakpoints.down("sm")]: {
+      width: "50%",
+    },
+  },
+}));
+export const DoughnutChart = ({
+  title,
+  legendPos,
+  legendDis,
+  cutout,
+  width,
+  height,
+  count,
+  countPosY,
+  totalPosY,
+  chartConfig,
+}) => {
+  const classes = useStyles();
+
+  let fontSize = 0.5;
+  let donutX;
+  let donutY = 0;
+  let cFontWeight = 200;
+
+  if (title && title.includes("DAILY")) {
+    fontSize = (height / 200).toFixed(2);
+    donutX = "DAILY";
+    donutY = 0.5 * height;
+    cFontWeight = 500;
+  } else if (title && title.includes("STAGE")) {
+    fontSize = (height / 300).toFixed(2);
+    donutX = "STAGING";
+    donutY = 0.59 * height;
+    cFontWeight = 400;
+  } else if (title && title.includes("RESPONSIBILITY")) {
+    fontSize = (height / 250).toFixed(2);
+    donutX = "RESPONSIBILITY";
+    donutY = 0.57 * height;
+    cFontWeight = 400;
+  } else {
+    fontSize = (height / 170).toFixed(2);
+    donutX = "DEFAULT";
+    donutY = 0.5 * height;
+    cFontWeight = 500;
+  }
+
+  return (
+    <div
+      className={classes.DoughnutChartWidth}
+      //   style={{ width: "35%" }}
+    >
+      <Doughnut
+        data={chartConfig}
+        options={{
+          cutout: "60%",
+          radius: "100%",
+          responsive: true,
+          maintainAspectRatio: true,
+          plugins: {
+            title: {
+              display: title ? true : false,
+              text: title,
+              textAlign: "left",
+            },
+            legend: {
+              labels: {
+                usePointStyle: true,
+                boxWidth: 9,
+              },
+              display: false,
+              // position: legendPos
+            },
+          },
+        }}
+        plugins={[
+          {
+            beforeDraw: function (chart) {
+              const width = chart.width,
+                //height = chart.height
+                ctx = chart.ctx;
+              ctx.restore();
+              ctx.font = `${fontSize}em sans-serif`;
+              ctx.textAlign = "center";
+              ctx.textBaseLine = "middle";
+              ctx.fillStyle = "#2c2b2c";
+              ctx.fontWeight = cFontWeight;
+
+              // Calculate text position
+
+              const textX = Math.round(width / 2);
+              const textY = donutY - totalPosY;
+              const countTextX = count ? textY + 50 + countPosY : textY;
+              const countTextY = count ? textY + 100 + countPosY : textY;
+              // Draw the text
+              ctx.fillText("All Inquiries", textX, textY);
+
+              ctx.fontWeight = cFontWeight;
+             //ctx.fillText(count, textX, countTextY, countTextX);
+              ctx.fillText(count, textX, countTextX, countTextY);
 
 
-    return (
-        <div style={{width: '35%' }}>
-            <Doughnut
-                data={chartConfig}
-                options={{
-                    cutout: "70%",
-                    radius: "97%",
-                    responsive: true,
-                    maintainAspectRatio: true,
-                    plugins: {
-                        title: {
-                            display: title ? true : false,
-                            text: title,
-                            textAlign: 'left',
-                        },
-                        legend: {
-                            labels: {
-                                usePointStyle: true,
-                                boxWidth: 9
-                            },
-                            display: false,
-                            // position: legendPos
-                        }
-                    }
-                }}
-                plugins={[{
-                    beforeDraw: function (chart) {
-                        const width = chart.width,
-                            //height = chart.height
-                            ctx = chart.ctx;
-                        ctx.restore();
-                        ctx.font = fontSize + "em sans-serif";
-                        ctx.textAlign = "center";
-                        ctx.textBaseLine = "middle";
-                        const text = count,
-                            textX = Math.round((width - ctx.measureText(text).width) / 2),
-                            textY = donutY;
-                        count ? ctx.fillText("Total", textX, textY - totalPosY) : ctx.fillText("No Data", textX - totalPosY, textY - totalPosY);
-                        ctx.fontWeight = cFontWeight;
-                        ctx.fillText(text, textX, textY - countPosY);
-                        ctx.save();
-                    }
-                }]
-                }
-            />
-         </div>
-    )
-}
-
-
-
+              ctx.save();
+            },
+          },
+        ]}
+      />
+    </div>
+  );
+};
