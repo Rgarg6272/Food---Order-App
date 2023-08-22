4c6d49f9d0fd696a5b42fd259c063a1099bc4de0
Rgarg6272 <73298337+Rgarg6272@users.noreply.github.com>
2023-08-22 18:32:41 +0530

    Update chart.js

--- chart.js
+++ chart.js
@@ -1,144 +1,181 @@
-import { Doughnut } from "react-chartjs-2";
-import {
-  Grid,
-  Container,
-  makeStyles,
-  Card,
-  TextField,
-  Typography,
-} from "@material-ui/core";
-// import { inquiriesStyles } from "../../css/InquiriesCss";
-const useStyles = makeStyles((theme) => ({
-  DoughnutChartWidth: {
-    width: "35%",
-    [theme.breakpoints.down(2100)]: {
-      width: "35%", //67%
-    },
+import React, { useState } from "react";
+import "./App.css";
+import MaterialTable from "material-table";
+import Button from "@material-ui/core/Button";
+import PersonAddDisabledIcon from "@material-ui/icons/PersonAddDisabled";
+import DelegatedDialog from "./component/DelegatedDialog";
 
-    [theme.breakpoints.down("lg")]: {
-      width: "60%", //75%
+function App() {
+  const [tableData, setTableData] = useState([
+    {
+      subscriberid: 87687676980,
+      jivamemberid: 98777,
+      memberfirstname: "Smith",
+      memberlastname: "John",
+      delegate: "PMS",
+      contacttype: "Care Team",
+      contactname: "Smith, John",
+      cellphone: 179458399,
+      workphone: 788437943,
+      email: "test1@gmail.com",
+      preferred: "Cell Phone",
     },
-
-    [theme.breakpoints.down(1750)]: {
-      width: "60%", //80%
+    {
+      subscriberid: 87687676980,
+      jivamemberid: 98777,
+      memberfirstname: "Tessa",
+      memberlastname: "Adam",
+      delegate: "Health Home",
+      contacttype: "Care Link",
+      contactname: "Tessa, Adam",
+      cellphone: 3645345453,
+      workphone: 4564564566,
+      email: "test2@gmail.com",
+      preferred: "Work Phone",
     },
-
-    [theme.breakpoints.down(1550)]: {
-      width: "60%", //90%
+    {
+      subscriberid: 87687676982,
+      jivamemberid: 98779,
+      memberfirstname: "Levi",
+      memberlastname: "Mark",
+      delegate: "PMG",
+      contacttype: "Not Found",
+      contactname: "Levi, Mark",
+      cellphone: "Not Found",
+      workphone: "Not Found",
+      email: "Not Found",
+      preferred: "Not Found",
     },
-
-    [theme.breakpoints.down(1400)]: {
-      width: "60%",
+    {
+      subscriberid: 87687676983,
+      jivamemberid: 98780,
+      memberfirstname: "Chris",
+      memberlastname: "Alister",
+      delegate: "PMS",
+      contacttype: "Unassigned",
+      contactname: "Unassigned",
+      cellphone: "-",
+      workphone: "-",
+      email: "-",
+      preferred: "-",
     },
-    [theme.breakpoints.down("md")]: {
-      width: "60%",
+  ]);
+  const columns = [
+    {
+      title: "Subscriber ID",
+      field: "subscriberid",
+      sorting: false,
+      filtering: false,
+      headerStyle: { color: "#fff" },
+      align: "center",
     },
-    [theme.breakpoints.down("sm")]: {
-      width: "50%",
+    {
+      title: "Jiva Member ID",
+      field: "jivamemberid",
+      filterPlaceholder: "filter",
+      align: "center",
     },
-  },
-}));
-export const DoughnutChart = ({
-  title,
-  legendPos,
-  legendDis,
-  cutout,
-  width,
-  height,
-  count,
-  countPosY,
-  totalPosY,
-  chartConfig,
-}) => {
-  const classes = useStyles();
+    {
+      title: "Member First Name",
+      field: "memberfirstname",
+      align: "center",
+      align: "center",
+    },
+    {
+      title: "Member Last Name",
+      field: "memberlastname",
+      align: "center",
+    },
+    {
+      title: "Delegate",
+      field: "delegate",
+      align: "center",
+    },
+    {
+      title: "Contact Type",
+      field: "contacttype",
+      filterPlaceholder: "filter",
+      align: "center",
+    },
+    {
+      title: "Contact Name",
+      field: "contactname",
+      headerStyle: { color: "#fff" },
+      align: "center",
+    },
+    {
+      title: "Cell Phone",
+      field: "cellphone",
+      filterPlaceholder: "filter",
+      align: "center",
+    },
+    {
+      title: "Work Phone",
+      field: "workphone",
+      filterPlaceholder: "filter",
+      align: "center",
+    },
+    {
+      title: "Email",
+      field: "email",
+      filterPlaceholder: "filter",
+      align: "center",
+    },
+    {
+      title: "Preferred",
+      field: "preferred",
+      filterPlaceholder: "filter",
+      align: "center",
+    },
+  ];
 
-  let fontSize = 0.5;
-  let donutX;
-  let donutY = 0;
-  let cFontWeight = 200;
+  const [dialogOpen, setDialogOpen] = useState(false);
 
-  if (title && title.includes("DAILY")) {
-    fontSize = (height / 200).toFixed(2);
-    donutX = "DAILY";
-    donutY = 0.5 * height;
-    cFontWeight = 500;
-  } else if (title && title.includes("STAGE")) {
-    fontSize = (height / 300).toFixed(2);
-    donutX = "STAGING";
-    donutY = 0.59 * height;
-    cFontWeight = 400;
-  } else if (title && title.includes("RESPONSIBILITY")) {
-    fontSize = (height / 250).toFixed(2);
-    donutX = "RESPONSIBILITY";
-    donutY = 0.57 * height;
-    cFontWeight = 400;
-  } else {
-    fontSize = (height / 170).toFixed(2);
-    donutX = "DEFAULT";
-    donutY = 0.5 * height;
-    cFontWeight = 500;
-  }
+  const handleDialog = () => {
+    setDialogOpen(true);
+  };
 
+  const handleCloseDialog = () => {
+    setDialogOpen(false);
+  };
   return (
-    <div
-      className={classes.DoughnutChartWidth}
-      //   style={{ width: "35%" }}
-    >
-      <Doughnut
-        data={chartConfig}
-        options={{
-          cutout: "60%",
-          radius: "100%",
-          responsive: true,
-          maintainAspectRatio: true,
-          plugins: {
-            title: {
-              display: title ? true : false,
-              text: title,
-              textAlign: "left",
-            },
-            legend: {
-              labels: {
-                usePointStyle: true,
-                boxWidth: 9,
-              },
-              display: false,
-              // position: legendPos
-            },
-          },
-        }}
-        plugins={[
+    <div className="App">
+      <MaterialTable
+        columns={columns}
+        data={tableData}
+        actions={[
           {
-            beforeDraw: function (chart) {
-              const width = chart.width,
-                //height = chart.height
-                ctx = chart.ctx;
-              ctx.restore();
-              ctx.font = `${fontSize}em sans-serif`;
-              ctx.textAlign = "center";
-              ctx.textBaseLine = "middle";
-              ctx.fillStyle = "#2c2b2c";
-              ctx.fontWeight = cFontWeight;
-
-              // Calculate text position
-
-              const textX = Math.round(width / 2);
-              const textY = donutY - totalPosY;
-              const countTextX = count ? textY + 50 + countPosY : textY;
-              const countTextY = count ? textY + 100 + countPosY : textY;
-              // Draw the text
-              ctx.fillText("All Inquiries", textX, textY);
-
-              ctx.fontWeight = cFontWeight;
-             //ctx.fillText(count, textX, countTextY, countTextX);
-              ctx.fillText(count, textX, countTextX, countTextY);
-
-
-              ctx.save();
-            },
+            icon: () => (
+              <>
+                <Button onClick={handleDialog}>Replace</Button>
+                <PersonAddDisabledIcon />
+              </>
+            ),
+            onClick: (e, data) => console.log(data),
           },
         ]}
+        options={{
+          sorting: true,
+          search: false,
+          searchFieldAlignment: "right",
+          searchAutoFocus: true,
+          searchFieldVariant: "standard",
+          paging: true,
+          pageSize: 5,
+          paginationType: "stepped",
+          showFirstLastPageButtons: false,
+          addRowPosition: "first",
+          actionsColumnIndex: -1,
+          columnsButton: true,
+          rowStyle: (data, index) =>
+            index % 2 === 0 ? { background: "#f5f5f5" } : null,
+          headerStyle: { background: "#f44336", color: "#fff" },
+        }}
+        title="Table"
       />
+      {dialogOpen && <DelegatedDialog handleCloseDialog={handleCloseDialog} />}
     </div>
   );
-};
+}
+
+export default App;
