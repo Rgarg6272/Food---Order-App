a3f63f5c1540a87d8e6960bff25c420121225051
Rgarg6272 <73298337+Rgarg6272@users.noreply.github.com>
2023-08-24 12:22:03 +0530

    Update dashboarddata.js

--- dashboarddata.js
+++ dashboarddata.js
@@ -9,8 +9,21 @@ import { Button, Paper } from "@material-ui/core";
 import Dialog from "@material-ui/core/Dialog";
 import DialogContent from "@material-ui/core/DialogContent";
 import Radio from "@material-ui/core/Radio";
+import TextField from "@material-ui/core/TextField";
 
 const useStyles = makeStyles((theme) => ({
+  root: {
+    "& > *": {
+      margin: "5px",
+      width: "25ch",
+    },
+  },
+  customInput: {
+    padding: "10px 12px",
+  },
+  customLabel: {
+    transform: "translate(14px, -6px) scale(0.75)",
+  },
   customToolbar: {
     "& .MTableToolbar-root-5": {
       minHeight: "0px !important",
@@ -145,6 +158,64 @@ const DelegatedDialog = ({
     },
   ];
 
+  const handleOpenTableDialog = () => {
+    console.log("Hey MF");
+    handleClose();
+  };
+
+  const assignContact = (
+    <>
+      <Grid container>
+        <Dialog open={open} onClose={handleClose} maxWidth="lg" fullWidth>
+          <DialogContent style={{ paddingTop: 0 }}>
+            <Grid container className={classes.typoHeaderContainer}>
+              <Grid item xs={8}>
+                <Typography className={classes.typoHeader}>
+                  Assign Delegated Contact
+                </Typography>
+              </Grid>
+              <Grid item xs={4} style={{ textAlign: "right" }}>
+                <CloseIcon
+                  className={classes.closeIcon}
+                  onClick={handleClose}
+                />
+              </Grid>
+            </Grid>
+            <Grid container>
+              <Grid item xs={12}>
+                <form className={classes.root} noValidate autoComplete="off">
+                  <TextField
+                    id="outlined-basic"
+                    label="Subscriber ID"
+                    variant="outlined"
+                    InputProps={{
+                      classes: { input: classes.customInput },
+                    }}
+                    InputLabelProps={{
+                      classes: { outlined: classes.customLabel },
+                      shrink: true,
+                    }}
+                  />
+                  <Button
+                    variant="contained"
+                    color="secondary"
+                    style={{
+                      borderRadius: 0,
+                      textTransform: "capitalize",
+                    }}
+                    onClick={handleOpenTableDialog}
+                  >
+                    Search Contact
+                  </Button>
+                </form>
+              </Grid>
+            </Grid>
+          </DialogContent>
+        </Dialog>
+      </Grid>
+    </>
+  );
+
   const body = (
     <>
       <Grid container>
@@ -221,7 +292,7 @@ const DelegatedDialog = ({
           aria-describedby="simple-modal-description"
           className={classes.modal}
         >
-          {body}
+          {assignContact}
         </Modal>
       </div>
     </React.Fragment>
