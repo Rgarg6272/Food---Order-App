00ef0a8f4c47b53a6da1af2be7e7e4ecd29d741a
Rgarg6272 <73298337+Rgarg6272@users.noreply.github.com>
2023-08-30 18:52:47 +0530

    Update reverse.js

--- reverse.js
+++ reverse.js
@@ -15,6 +15,7 @@ import { DelegatedContactData, AdDelegateContactData } from "../../../constants/
 import { commonFontSizes } from "../../css/FontSizes";
 import { TablePagination } from "@material-ui/core";
 import MaterialTable, { MTableToolbar } from "material-table";
+import { Column } from "jspdf-autotable";
 //import { table } from "console";
 
 
@@ -268,13 +269,130 @@ const AssignDeleTableDialog = ({ handleDeleTableClose, handleAddRow }) => {
     const handleAssign = () => {
         setOpen(false);
         setSearchDialogOpen(true);
+
+        // if(selectedRow) {
+        //     const newRow = {...selectedRow};
+        //     const missingColumns = mainTableColumns
+        //          .filter(column => !newRow[column.field])
+        //          .map(column => column.field);
+        //     missingColumns.forEach(column => {
+        //         newRow[column] = "-";
+        //     });
+        //     handleAddRow(newRow);
+
+        // }
     };
 
     const handleAlertDeleClose = () => {
         setSearchDialogOpen(false);
     }
 
-    const body = (
+    const Replacebody = (
+        <>
+         <Grid container>
+                <Dialog
+                    open={open}
+                    onClose={() => setSearchDialogOpen(false)}
+                    maxWidth="lg"
+                    fullWidth
+                >
+                    <DialogContent style={{ paddingTop: 0, paddingBottom: '30px' }}>
+                        <Grid container className={classes.typoHeaderContainer}>
+                            <Grid item xs={8}>
+                                <Typography className={classes.typoHeader}>
+                                    Replace Delegated Contact
+                                </Typography>
+                            </Grid>
+                            <Grid item xs={4} style={{ textAlign: "right" }}>
+                                <CloseIcon
+                                    className={classes.closeIcon}
+                                    onClick={handleClose}
+                                />
+                            </Grid>
+                        </Grid>
+                        <Grid container>
+                            <Grid item xs={12}>
+                                <Paper elevation={0} className={classes.contentPaper}>
+                                    <div className={classes.customToolbar}>
+                                        <MuiThemeProvider theme={theme}>
+                                            <div className="tableContainer1" style={tableStyle}>
+                                                <MaterialTable
+                                                    autoHeight={true}
+                                                    key={count}
+                                                    data={tableData} 
+                                                    columns={columns}
+                                                    options={{
+                                                        paging:false,
+                                                        search: false,
+                                                        toolbar: false,
+                                                        sorting: false,
+                                                        detailPanelType: "single",
+                                                        selection: false,
+                                                        maxBodyHeight: "40vh",
+                                                        overflowY: "hidden !important",                                             
+                                                        padding: "dense",
+                                                        filtering: false,
+                                                        showTitle: false,
+                                                        doubleHorizontalScroll: false,
+                                                        headerStyle: {
+                                                            whiteSpace: "nowrap",
+                                                            position: "sticky",
+                                                            fontWeight: 700,
+                                                            fontSize: commonFontSizes.bodyTwo + "rem",
+                                                            color: "#2C2B2C",
+                                                            border: "0px solid lightgrey",
+                                                            textAlign: "start"
+                                                        },
+                                                        cellStyle: () => CellBorderStyle,
+                                                        rowStyle: (row) => {
+                                                            const id = row.tableData.id;
+                                                            return {
+                                                                backgroundColor: id%2 === 0 ? "#F5F5F5" : "#fff",
+                                                                borderBottom: "1px solid lightgray",
+                                                                borderTop: id === 0 ? "1px solid lightgray" : "none",
+                                                            }
+                                                        },
+                                                    }}
+                                                />
+                                            </div>
+                                        </MuiThemeProvider>
+                                    </div>
+                                </Paper>
+                            </Grid>
+                        </Grid>
+                    </DialogContent>
+                    <Grid container>
+                        <Grid
+                            item
+                            xs={12}
+                            style={{
+                                display: "flex",
+                                justifyContent: "end",
+                                marginRight: "25px",
+                                position: 'relative',
+                                bottom: '17px'
+                            }}
+                        >
+                            <Button
+                                variant="contained"
+                                color="secondary"
+                                style={{
+                                    borderRadius: 0,
+                                    height: "30px",
+                                    textTransform: "capitalize",
+                                    backgroundColor: '#217e76'
+                                }}
+                            >
+                                Replace
+                            </Button>
+                        </Grid>
+                    </Grid>
+                </Dialog>
+            </Grid>
+        </>
+    );
+
+    const Assignbody = (
         <>
             <Grid container>
                 <Dialog
@@ -450,7 +568,7 @@ const AssignDeleTableDialog = ({ handleDeleTableClose, handleAddRow }) => {
                 aria-describedby="simple-modal-description"
                 className={classes.modal}
             >
-                {body}
+                {Assignbody}
             </Modal>
             {searchDialogOpen && <AlertDeleDialog handleAlertDeleClose={handleAlertDeleClose} handleAddRow={handleAddRow}
                 selectedRowData={selectedRow} />}
