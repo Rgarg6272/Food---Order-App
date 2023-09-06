c920f230e1919c6c953c653ef8553ffb6c2e73f8
Rgarg6272 <73298337+Rgarg6272@users.noreply.github.com>
2023-09-07 00:27:00 +0530

    Update nando.js

--- nando.js
+++ nando.js
@@ -1,47 +1,27 @@
 #include <iostream>
-
 using namespace std;
-
-int main()
-{
-    int arr[100];
-    int n;
+int main() {
+    int arr[5];
     int len;
+    int num;
+    int pos;
+    cout<<"Enter the length: ";
+    cin>>len;
+    cout<<"Enter the Elements: ";
     
-    cout << "Enter the length: ";
-    cin >> len;
-
-    cout << "Enter the elements: ";
-    
-    for (int i = 0; i < len; i++) {
-        cin >> arr[i];
-    }
-
-    n = len;  // Assign len to n
-
-    for (int i = 0; i < n - 1; i++) {
-        
-        int minIndex = i;  // Use consistent variable name
-        for (int j = i + 1; j < n; j++) {
-            if (arr[j] < arr[minIndex]) {
-                minIndex = j;
-            }
-        }
-
-        // Swap arr[i] and arr[minIndex]
-
-        int temp = arr[i];
-        arr[i] = arr[minIndex];
-        arr[minIndex] = temp;
+    for(int i=0; i<len; i++){
+        cin>>arr[i];
     }
     
-    cout << "Selection sort result:" << endl;
-    
-    for (int i = 0; i < n; i++) {
-        cout << arr[i] <<" ";
+    cout<<"Elements of Array: "<<endl;
+    for(int i=0; i<len; i++){
+        cout<<arr[i]<<" ";
     }
-
-
+    cout<<endl;
+    
+    cout<<"enter the element to be insert: ";
+    cin>>num;
+    cout<<"enter the position of insertion: ";
+    cin>>pos;
     return 0;
-
 }
